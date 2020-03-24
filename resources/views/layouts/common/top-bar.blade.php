298584fdb591ade827797e1a32f4413dc05d405e
LEanhNhan0410 <32216722+LEanhNhan0410@users.noreply.github.com>
2020-03-24 23:29:13 +0700

    commit authentication v1.0

---
+++ resources/views/layouts/common/top-bar.blade.php
@@ -0,0 +1,120 @@
+
+<div id="top-bar" class="">
+    <div class="container-fluid clearfix">
+
+        <div class="row col_half col_last left nobottommargin top_link_wrapper">
+
+            <!-- Top Links
+            ============================================= -->
+            <div class="top-links">
+                <ul class="sf-js-enabled clearfix" style="touch-action: pan-y;">
+                    <!-- ngRepeat: item in Frontend_TopLinkHeaderLeft --><li class="sfHover ng-scope" ng-repeat="item in Frontend_TopLinkHeaderLeft">
+                        <a ng-href="/#" class="ng-binding" href="/#">
+                            <img ng-show="item.ImageURL!=null" ng-src="/Images/Webcontent/75a67d34-4c24-4383-8c52-7777d99a11c3.png" width="15" src="/Images/Webcontent/75a67d34-4c24-4383-8c52-7777d99a11c3.png">
+                            Đổi trả hàng trong 7 ngày
+                        </a>
+                    </li><!-- end ngRepeat: item in Frontend_TopLinkHeaderLeft --><li class="sfHover ng-scope" ng-repeat="item in Frontend_TopLinkHeaderLeft">
+                        <a ng-href="/#" class="ng-binding" href="/#">
+                            <img ng-show="item.ImageURL!=null" ng-src="/Images/Webcontent/6c7c555c-4ab0-4060-91cf-22b8581c093a.png" width="15" src="/Images/Webcontent/6c7c555c-4ab0-4060-91cf-22b8581c093a.png">
+                            Giao hàng tận nơi
+                        </a>
+                    </li><!-- end ngRepeat: item in Frontend_TopLinkHeaderLeft --><li class="sfHover ng-scope" ng-repeat="item in Frontend_TopLinkHeaderLeft">
+                        <a ng-href="/#" class="ng-binding" href="/#">
+                            <img ng-show="item.ImageURL!=null" ng-src="/Images/Webcontent/957f5c17-1f0a-4973-97dc-0c52fe165c38.png" width="15" src="/Images/Webcontent/957f5c17-1f0a-4973-97dc-0c52fe165c38.png">
+                            Bảo hành chu đáo
+                        </a>
+                    </li><!-- end ngRepeat: item in Frontend_TopLinkHeaderLeft --><li class="sfHover ng-scope" ng-repeat="item in Frontend_TopLinkHeaderLeft">
+                        <a ng-href="/#" class="ng-binding" href="/#">
+                            <img ng-show="item.ImageURL!=null" ng-src="/Images/Webcontent/9cfb4166-dfa7-4280-94d4-36043b6dc969.png" width="15" src="/Images/Webcontent/9cfb4166-dfa7-4280-94d4-36043b6dc969.png">
+                            Chấp nhận thanh toán visa/master
+                        </a>
+                    </li><!-- end ngRepeat: item in Frontend_TopLinkHeaderLeft --><li class="sfHover ng-scope" ng-repeat="item in Frontend_TopLinkHeaderLeft">
+                        <a ng-href="/#" class="ng-binding" href="/#">
+                            <img ng-show="item.ImageURL!=null" ng-src="" width="15" class="ng-hide">
+                            Số 7 Hồ Bá Kiện, Phường 15, Quận 10, TPHCM
+                        </a>
+                    </li><!-- end ngRepeat: item in Frontend_TopLinkHeaderLeft -->
+
+                </ul>
+            </div><!-- .top-links end -->
+
+        </div>
+        <div class="col_half col_last fright nobottommargin top_link_wrapper">
+
+            <!-- Top Links
+            ============================================= -->
+            <div class="top-links">
+                <ul class="sf-js-enabled clearfix has-user-info" style="touch-action: pan-y;">        <li class="sfHover account-link">
+        <a><i class="fa fa-sign-in"></i> Đăng nhập</a>
+        <div class="login-form account-form">
+
+
+
+<form action="/dang-nhap.html" method="post"><input name="__RequestVerificationToken" type="hidden" value="Ulpuq9WFWK5IoqI_CyRtFtIj0vz7xzeyGFMzEM8x47c8YQW7x6KCK2-vqU-sczWF0RInXU_16kuPVPFdPxCs91FQlvlpT-3wxajubOPF2DxLTJJTDQn5oWVR73rhXlrNi5BexT8wR63zq3sfUaDxirXXn1cyRw2hn-zw6KjT2R01">                    <div>
+                    <input class="text-box" id="UserName" name="UserName" placeholder="Nhập Email" type="text" value="">
+
+                    <input class="text-box" id="Password" name="Password" placeholder="Mật khẩu" type="password" value="">
+
+
+                </div>
+                <div>
+                    <div class="left">
+                        <input type="submit" value="Đăng nhập" class="submit-button">
+                    </div>
+                    <div class="right">
+                        <a href="javascript:void(0);" class="social-button btnLoginFacebook"><i class="fa fa-facebook"></i></a>
+                        <a href="javascript:void(0);" class="social-button btnLoginGoogle"><i class="fa fa-google-plus"></i></a>
+                    </div>
+                </div>
+</form>            </div>
+    </li>
+    <li class="sfHover account-link">
+        <a><i class="fa fa-user-plus"></i> Đăng ký</a>
+        <div class="register-form account-form">
+<form action="/dang-ky.html" method="post"><input name="__RequestVerificationToken" type="hidden" value="SqFd57uThDQXet3Zv3prtBTh-Qp-do7urAhh-nofO5RaB_GBYd2vFnmaUOIuyC2_1HpCjHuDiz8nJGve0Mf4ewZOX59gxDnuZV8_n0it8wZfhHeTyJKOIomSFZFI2xgj4fjM0ALq8SIsmkJr_KArUoPE67JQWguruPuXsB6tADA1">                    <div>
+
+                    <label class="has-error"></label>
+                    <span class="field-validation-valid" data-valmsg-for="FullName" data-valmsg-replace="true"></span>
+                    <input class="text-box required-entry form-control" id="FullName" name="FullName" placeholder="Họ tên" type="text" value="">
+
+
+                    <span class="field-validation-valid" data-valmsg-for="UserName" data-valmsg-replace="true"></span>
+                    <input class="text-box required-entry form-control email" id="UserName" name="UserName" placeholder="Tên tài khoản" type="text" value="">
+
+
+                    <span class="field-validation-valid" data-valmsg-for="Birthday" data-valmsg-replace="true"></span>
+                    <input class="text-box form-control" id="Birthday" name="Birthday" placeholder="mm/dd/yyyy" type="date" value="">
+
+
+                    <span class="field-validation-valid" data-valmsg-for="Password" data-valmsg-replace="true"></span>
+                    <input class="text-box required-entry validate-password form-control" id="Password" name="Password" placeholder="Mật khẩu" type="password" value="">
+
+
+
+                    <span class="field-validation-valid" data-valmsg-for="ConfirmPassword" data-valmsg-replace="true"></span>
+                    <input class="text-box required-entry validate-password form-control" id="ConfirmPassword" name="ConfirmPassword" placeholder="Mật khẩu" type="password" value="">
+
+                </div>
+                <div>
+                    <input type="submit" value="Đăng ký" class="submit-button">
+                </div>
+                <div class=" fast-login">
+                    Bạn đã có tài khoản? <a href="javascript:login_register.toLogin();">Đăng nhập</a> ngay!
+                </div>
+</form>            </div>
+    </li>
+
+
+
+                    <!-- ngRepeat: item in Frontend_TopLinkHeader -->
+
+
+
+                </ul>
+            </div><!-- .top-links end -->
+
+        </div>
+
+    </div>
+
+</div>
